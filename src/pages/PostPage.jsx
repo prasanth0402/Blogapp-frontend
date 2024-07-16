0af5deef619bbeaf5ff8c524ae6e37e831c38f1c
prasanth0402 <prasanth1359d@gmail.com>
2024-07-16 10:29:41 +0530

    postpage change

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -7,7 +7,7 @@ const PostPage = () => {
         <h1 className=" text-2xl font-bold">Explore technology</h1>
         <hr class="w-[70%] h-1 mx-auto border-0 rounded bg-gray-700" />
       </div>
-      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 ">
         <Posts />
         <Posts />
         <Posts />
